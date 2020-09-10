e16fd07e9020e5cc1a7d6f2f2d3284253d24938d
MBR <buildmachineubuntu@gmail.com>
2020-09-10 11:14:38 +0200

    big cleanup

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let body = document.getElementsByTagName('body')[0];
-
-    if (body.className == "" && !window.nomax) body.className = "maxheight";
-
     document.getElementById("menu").onclick = () => {
         let elements = document.querySelectorAll(".show");
         if (elements.length > 0) {
-            if (body.className == "maxheight" && !window.nomax) body.className = "";
             elements.forEach(x => x.className = "hide");
             return;
         }
-
-        if (body.className == "" && !window.nomax) body.className = "maxheight";
     
         elements = document.querySelectorAll(".hide");
         elements.forEach(x => {
             if (x.id != "menu") x.className = "show";
         });
     };
+
+    let days_since_alert_dismissed = (Date.now() - Number(localStorage.getItem("last_alert_time"))) / 1000 / 60 / 60 / 24;
+    if (days_since_alert_dismissed >= 30) {
+        let element = document.querySelectorAll(".alert")[0];
+        element.classList.remove("hidden");
+    }
 }, false);
+
+function RemoveAlert(automatic = false) {
+    let element = document.querySelectorAll(".alert")[0];
+    element.classList.add("hidden");
+
+    if (!automatic) localStorage.setItem("last_alert_time", Date.now());
+}
